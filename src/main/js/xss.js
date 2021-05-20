af39157c36bdd8a5e683cc645f16dc281bd1a7b5
A-beidas <a.m.beidas@gmail.com>
2021-05-21 00:51:55 +0300

    modifying variable naming

--- src/main/js/xss.js
+++ src/main/js/xss.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
-const Scan = () => {
-    const [scanURL, setScanURL] = useState("http://localhost/xss/index.php");
+const XSS = () => {
+    const [targetURL, setTargetURL] = useState("http://localhost/xss/index.php");
     const [scanResult, setScanResult] = useState([]);
     function scan() {
       event.preventDefault();
@@ -11,7 +11,7 @@ const Scan = () => {
                      Authorization: 'Bearer ' + localStorage.getItem('bearer-token') }
       };
       var url = new URL('http://localhost:81/api/xss')
-      url.search = new URLSearchParams({url: scanURL}).toString();
+      url.search = new URLSearchParams({url: targetURL}).toString();
       fetch(url, requestOptions)
           .then(function(response) {
               return response.text();
@@ -25,7 +25,7 @@ const Scan = () => {
     return (
       <div>
           <form onSubmit={ scan }>
-              <input type="input"  value={scanURL} onChange={e => setScanURL(e.target.value)} name="url"/>
+              <input type="input"  value={targetURL} onChange={e => setTargetURL(e.target.value)} name="url"/>
               <input type="submit" value="Scan"/>
           </form>
           <div>
