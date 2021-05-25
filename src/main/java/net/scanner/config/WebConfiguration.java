d991c6c863889f18ae1e79a7bb7a9c4dca1e450a
A-beidas <a.m.beidas@gmail.com>
2021-05-25 14:26:46 +0300

    Necessary file for resolving frontend urls

---
+++ src/main/java/net/scanner/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package net.scanner.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("forward:/");
+        registry.addViewController("/register").setViewName("forward:/");
+        registry.addViewController("/scan").setViewName("forward:/");
+        registry.addViewController("/error").setViewName("forward:/");
+    }
+}
