03d3c3cf4fdd0854ae42cf3bfd44c90d20c989b7
A-beidas <a.m.beidas@gmail.com>
2021-06-02 04:52:57 +0300

    http request changes

--- src/main/js/components/Crawl.js
+++ src/main/js/components/Crawl.js
@@ -1,26 +1,28 @@
 import React, { useState } from 'react';
-
 import { Form, Button } from "react-bootstrap";
+import axios from 'axios';
 
 const Crawl = () => {
     const [targetURL, setTargetURL] = useState("http://192.168.56.101/bWAPP/login.php");
     const [scanResult, setScanResult] = useState([]);
     function scan(event) {
       event.preventDefault();
-      const requestOptions = {
-          method: 'GET',
-          headers: { "Content-Type": "application/json",
-                     Authorization: 'Bearer ' + localStorage.getItem('bearer-token') }
-      };
-      var url = new URL('http://localhost:81/api/scan')
-      url.search = new URLSearchParams({url: targetURL}).toString();
-      fetch(url, requestOptions)
-          .then(function(response) {
-              return response.text();
-          })
-          .then(result => {
-              setScanResult(JSON.parse(result));
-          })
+      const config = {
+          headers:
+            { Authorization: 'Bearer ' + localStorage.getItem('bearer-token') },
+          params:
+            { url: targetURL }
+          };
+      axios.get('/api/scan', config)
+        .then(response => {
+          if (response.status != 200) {
+              throw response;
+          }
+          setScanResult(response.data);
+        })
+        .catch (error_response => {
+          setScanResult(error_response.data);
+        })
     }
     return (
       <div>
@@ -29,7 +31,7 @@ const Crawl = () => {
                     <Form.Label>URL</Form.Label>
                     <Form.Control onChange={e => setTargetURL(e.target.value)} type="text" name="url" value={targetURL}/>
                 </Form.Group>
-				<Button type="submit">Begin Crawl</Button>
+				<Button type="submit">Begin Scan</Button>
 			</Form>
           <div>
             {/* {scanResult.map(line => (<div>{line}<br/></div>))} */}
--- src/main/js/components/XSS.js
+++ src/main/js/components/XSS.js
@@ -1,35 +1,37 @@
 import React, { useState } from 'react';
-
 import { Form, Button } from "react-bootstrap";
+import axios from 'axios';
 
 const XSS = () => {
     const [targetURL, setTargetURL] = useState("http://localhost/xss/index.php");
     const [scanResult, setScanResult] = useState([]);
-    function scan(event) {
+    function xss(event) {
       event.preventDefault();
-      const requestOptions = {
-          method: 'GET',
-          headers: { "Content-Type": "application/json",
-                     Authorization: 'Bearer ' + localStorage.getItem('bearer-token') }
+      const config = {
+          headers:
+            { Authorization: 'Bearer ' + localStorage.getItem('bearer-token') },
+          params:
+            { url: targetURL }
       };
-      var url = new URL('http://localhost:81/api/xss')
-      url.search = new URLSearchParams({url: targetURL}).toString();
-      fetch(url, requestOptions)
-          .then(function(response) {
-              return response.text();
+      axios.get('/api/xss', config)
+          .then(response => {
+            if (response.status != 200) {
+                throw response;
+            }
+            setScanResult(response.data);
           })
-          .then(result => {
-              setScanResult(JSON.parse(result));
+          .catch (error_response => {
+            setScanResult(error_response.data);
           })
     }
     return (
       <div>
-          <Form onSubmit={ scan }  style={{width: 300}}>
+          <Form onSubmit={ xss }  style={{width: 300}}>
 				<Form.Group>
                     <Form.Label>URL</Form.Label>
                     <Form.Control onChange={e => setTargetURL(e.target.value)} type="text" name="url" value={targetURL}/>
                 </Form.Group>
-				<Button type="submit">Scan</Button>
+				<Button type="submit">Check URL</Button>
 			</Form>
           <div>
             {/* {scanResult.map(line => (<div>{line}<br/></div>))} */}
