4d638bb2e1fc655b50988ee13689c7a060ba624a
A-beidas <a.m.beidas@gmail.com>
2021-06-18 13:43:02 +0300

    joining controllers for core scanning

--- src/main/java/net/scanner/controller/ScanController.java
+++ src/main/java/net/scanner/controller/ScanController.java
@@ -1,7 +1,9 @@
 package net.scanner.controller;
 
+import net.scanner.model.Alert;
 import net.scanner.model.Scan;
 import net.scanner.service.ScanService;
+import net.scanner.service.XssService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +20,18 @@ public class ScanController {
     @Autowired
     ScanService scanService;
 
+    @Autowired
+    XssService xssService;
 
     @GetMapping(path = "/scan")
     public Scan scanPage(@RequestParam String url) throws IOException, URISyntaxException {
         Scan result = scanService.crawl(url);
+        return result;
+    }
+
+    @GetMapping(path = "/xss")
+    Alert xss(@RequestParam String url) throws IOException, URISyntaxException {
+        Alert result = xssService.xss(url);
         return result;
     }
 }
