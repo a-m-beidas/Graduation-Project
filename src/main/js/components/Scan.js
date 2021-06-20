de7db116aa6e0fcd58a719e18a155406e9c20583
A-beidas <a.m.beidas@gmail.com>
2021-06-20 18:46:56 +0300

    scan style

--- src/main/js/components/Scan.js
+++ src/main/js/components/Scan.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Form, Button, Container } from "react-bootstrap";
+import { Form, Button, Container, Image, Collapse } from "react-bootstrap";
+import Switch from 'react-switch';
 import { useHistory } from "react-router-dom";
 import axios from 'axios';
 
@@ -8,6 +9,7 @@ const Scan = () => {
     const [targetURL, setTargetURL] = useState("example");
     const history = useHistory();
     const [requestMessage, setRequestMessage] = useState("")
+    const [[cops1, setCops1], [cops2, setCops2], [cops3, setCops3]] = [useState(false), useState(false), useState(false)];
     const [scanResult, setScanResult] = useState({
       "id": 1,
       "userId": 24,
@@ -67,13 +69,67 @@ const Scan = () => {
         })
     }
     return (
-      <div>
+      <div className="div-scan">
+        <div>
+            <Image roundedCircle width="250" src="astronaut.jpg"/>
+        </div><br/>
         <Form onSubmit={ scan } className="form-app">
-          <Form.Group>
+          <Form.Group style={{position: "relative"}} className="d-flex justify-content-between">
             <Form.Label>URL</Form.Label>
-            <Form.Control onChange={e => setTargetURL(e.target.value)} type="text" name="url" value={targetURL}/>
+            <Form.Control className="rounded-pill form-control" onChange={e => setTargetURL(e.target.value)} type="text" name="url" value={targetURL}/>
+            <Button className="form-on-button rounded-pill" type="submit">Begin Scan</Button>
           </Form.Group>
-          <Button type="submit">Begin Scan</Button>
+          
+          {/* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */}
+          <Form.Group style={{backgroundColor: "white"}}>
+            <div className="d-flex px-2 justify-content-between">
+              <h4>Login Credentials</h4>
+              <Switch height={20} width={45} onColor={"#007BFF"} checkedIcon={false} uncheckedIcon={false} onChange={() => setCops1(!cops1)} checked={cops1}/>
+            </div>
+            <Collapse in={cops1}>
+              <div className="py-4">
+                <div className="d-flex px-2 justify-content-between">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control className="grey-control-scan" style={{width: "70%"}}/>
+                  </div>
+                  <br/>
+                  <div className="d-flex px-2 justify-content-between">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control className="grey-control-scan" style={{width: "70%"}}/>
+                  </div>
+              </div>
+            </Collapse>
+          </Form.Group>
+          
+          {/* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */}
+          <Form.Group style={{backgroundColor: "white"}}>
+            <div className="d-flex px-2 justify-content-between">
+              <h4>Session ID</h4>
+              <Switch onColor={"#007BFF"} checkedIcon={false} uncheckedIcon={false} onChange={() => setCops2(!cops2)} checked={cops2}/>
+            </div>
+            <Collapse in={cops2}>
+              <div className="py-4">
+                <div className="d-flex" style={{flexFlow: "column wrap", alignContent: "space-around"}}>
+                  <Form.Control className="grey-control-scan" style={{width: "70%"}}/>
+                </div>
+              </div>
+            </Collapse>
+          </Form.Group>
+
+          {/* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */}
+          <Form.Group style={{backgroundColor: "white"}}>
+            <div className="d-flex px-2 justify-content-between">
+              <h4>Cookie</h4>
+              <Switch onColor={"#007BFF"} checkedIcon={false} uncheckedIcon={false} onChange={() => setCops3(!cops3)} checked={cops3}/>
+            </div>
+            <Collapse in={cops3}>
+              <div className="py-4">
+                <div className="d-flex" style={{flexFlow: "column wrap", alignContent: "space-around"}}>
+                  <Form.File/>
+                </div>
+              </div>
+            </Collapse>
+          </Form.Group>
         </Form>
         { requestMessage }
       </div>
