83b5052d4883a85a2d5720e68f2ab576d3e7961c
A-beidas <a.m.beidas@gmail.com>
2021-06-27 01:04:26 +0300

    routes and paths fix

--- Graduation-Project/src/main/js/App.js
+++ Graduation-Project/src/main/js/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import PrivateRoute from './routes/PrivateRoute';
 import LogoutRoute from './routes/LogoutRoute';
-import Home from './urls/DashboardPath';
+import Home from './urls/Home';
 import LogIn from "./urls/LogIn";
 import Register from "./urls/Register";
 import Scan from "./urls/Scan";
 import Report from "./urls/Report";
 import Alert from './urls/Alert'
 import Error from "./urls/Error";
+import DashboardPath from './urls/DashboardPath';
 
 const App = () => {
 
@@ -26,7 +27,7 @@ const App = () => {
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav style={{ textAlign: "right" }}>
-                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href={isLoggedIn ? "/dashboard": "/"}>Home</Nav.Link>
                         <Nav.Link hidden={isLoggedIn} href="/login">Login</Nav.Link>
                         <Nav.Link hidden={isLoggedIn} href="/register">Register</Nav.Link>
                         <Nav.Link hidden={!isLoggedIn} href="/scan">Scan</Nav.Link>
@@ -37,13 +38,14 @@ const App = () => {
             {/* <Container className="px-4" fluid> */}
             {/* <div style={{ marginTop: "10px" }}> */}
             <Switch>
-                <Route component={Home} path="/" exact />
+                <Route component={isLoggedIn ? DashboardPath : Home} path="/" exact />
                 <Route path="/login" exact render={(props) => (
                     <LogIn setLogIn={setLogIn} />
                 )} />
                 <Route component={Register} path="/register" />
                 <Route component={Error} path="/error" />
-                <PrivateRoute component={Scan} path="/scan" />
+                <PrivateRoute component={DashboardPath} path="/dashboard"/>
+                <PrivateRoute component={Scan} path="/scan"/>
                 <PrivateRoute component={Report} path="/report" />
                 <PrivateRoute component={Report} path="/report:id" />
                 <PrivateRoute component={Alert} path="/alert" />
